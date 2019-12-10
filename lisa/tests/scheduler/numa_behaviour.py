bda57e6e763bfb4d9f44da330e49f57b3358570c
Peter Puhov <peter.puhov@linaro.org>
2019-12-10 10:32:28 -0500

    Change to use trace.analysis.tasks code

--- lisa/tests/scheduler/numa_behaviour.py
+++ lisa/tests/scheduler/numa_behaviour.py
@@ -50,9 +50,8 @@ class NUMABehaviour(RTATestBundle):
         :returns: A Pandas DataFrame for the task, showing the
                   CPU's that the task was migrated to
         """        
-        df = self.trace.df_events('sched_switch')[['next_pid', 'next_comm', '__cpu']]
-        cpu_df = df_filter_task_ids(df, [task_id], pid_col='next_pid', comm_col='next_comm')
-        cpu_df = df_deduplicate(cpu_df, cols=['__cpu'], keep='first', consecutives=True)
+        df = self.trace.analysis.tasks.df_task_states(task_id)
+        cpu_df = df_deduplicate(df, cols=['cpu'], keep='first', consecutives=True)
                 
         return cpu_df
 
